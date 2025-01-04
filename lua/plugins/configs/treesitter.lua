b467b9a3d835475b39b08683c130546919533cc3
isa <isa080199@gmail.com>
2025-01-04 14:13:20 +0100

    init

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,32 @@
+local options = {
+
+  ensure_installed = {
+    "bash",
+    "c",
+    "diff",
+    "jsdoc",
+    "json",
+    "jsonc",
+    "lua",
+    "luadoc",
+    "luap",
+    "markdown",
+    "markdown_inline",
+    "printf",
+    "python",
+    "regex",
+    "vim",
+    "vimdoc",
+    "xml",
+    "yaml",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
