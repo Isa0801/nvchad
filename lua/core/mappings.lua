72fcfaf755941b79ff5e02c1c296ef40cbfa2f30
isa <isa080199@gmail.com>
2025-01-04 14:24:10 +0100

    change key mapping to exit term

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -55,7 +55,7 @@ M.general = {
   },
 
   t = {
-    ["<C-x>"] = { vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), "Escape terminal mode" },
+    ["<C-q>"] = { vim.api.nvim_replace_termcodes("<C-\\><C-N> :q<CR>", true, true, true), "Escape terminal mode" },
   },
 
   v = {
